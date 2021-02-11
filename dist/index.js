9259da19d221b9008b53a78125bf7870f14e5da6
itBankstown <78767220+itBankstown@users.noreply.github.com>
2021-02-12 09:07:56 +1100

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -21,7 +21,10 @@ const AlertDialog = ({ close, message, options }) => {
                         action.onClick();
                         close();
                     }, color: "primary", key: index }, action.copy))),
-            react_1.default.createElement(Button_1.default, { onClick: close, color: "primary" }, options.closeCopy || "Okay"))));
+            if(options.hide){
+            } else {
+                react_1.default.createElement(Button_1.default, { onClick: close, color: "primary" }, options.closeCopy || "Okay"))));
+            }
 };
 exports.default = AlertDialog;
 //# sourceMappingURL=index.js.map
